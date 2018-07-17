92de2b4dd45feac13d46557c41f5ff9faab24976
Ebraheem1 <ebraheem.mouhamed@gmail.com>
2018-07-17 21:12:33 +0200

    Modifying the admin password

--- seeds/therapists.js
+++ seeds/therapists.js
@@ -1,26 +1,8 @@
-var bcrypt = require('bcrypt');
-var pas = encryptByBcrypt('123');
-
-function encryptByBcrypt(password)
-{
-  bcrypt.hash(password, parseInt(process.env.SALT), function(err, hash)
-  {
-    if(err)
-    {
-      encryptByBcrypt(password);
-    }
-    else{
-      return hash;
-    }
-  });
-}
-
-
 const mainTherapist =[
 {
   name: 'T1',
   email: 'first@theraleap.com',
-  password: '$2b$10$Puo0.eHAT5sAaRKrdRykj.eZlOJMB3tmPliKRA5NFkYrW9IEqxzve'
+  password: '$2b$10$4o0u5EXk36ByRibZkZ4UvewNrsAoumfHZT5YoeaUO2uvb72/YDpAO'
 }
 ];
 
