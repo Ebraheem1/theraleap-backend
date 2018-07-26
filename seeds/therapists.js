b3e771d2d12674e77a2664add3b7ca3dcaaf3d15
Ebraheem1 <ebraheem.mouhamed@gmail.com>
2018-07-26 11:30:39 +0200

    Now we delete the credentials to make the system publice

--- seeds/therapists.js
+++
@@ -1,9 +0,0 @@
-const mainTherapist =[
-{
-  name: 'T1',
-  email: 'first@theraleap.com',
-  password: '$2b$10$4o0u5EXk36ByRibZkZ4UvewNrsAoumfHZT5YoeaUO2uvb72/YDpAO'
-}
-];
-
-module.exports = mainTherapist;
